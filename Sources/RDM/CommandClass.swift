34f4ad70e29aa986b5f026dcebc017754c964651
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-03-12 10:08:54 -0500

    Added `CommandClass`

---
+++ Sources/RDM/CommandClass.swift
@@ -0,0 +1,30 @@
+//
+//  CommandClass.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 3/12/20.
+//
+
+/// RDM Command Classes
+///
+/// - SeeAlso: ANSI E1.20 – 2010, page 102, Table A-1: Command Class Defines
+public enum CommandClass: UInt8, Codable {
+    
+    /// Discovery command
+    case discovery              = 0x10
+    
+    /// Discovery Response command
+    case discoveryResponse      = 0x11
+    
+    /// Get command
+    case get                    = 0x20
+    
+    /// Get Response command
+    case getResponse            = 0x21
+    
+    /// Set command
+    case set                    = 0x30
+    
+    /// Set Response comand
+    case setResponse            = 0x31
+}
