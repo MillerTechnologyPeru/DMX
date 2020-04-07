6ffd56485b9bf2c706024bccdbd2da7ac29072a6
Jurjdev <jurjdev@gmail.com>
2020-04-07 13:27:33 -0500

    Added `GetQueueMessage` Unit Tests

--- Tests/RDMTests/RDMMessageTest.swift
+++ Tests/RDMTests/RDMMessageTest.swift
@@ -13,6 +13,7 @@ final class RDMMessageTest: XCTestCase {
     
     static let allTests = [
         ("testDataCheckSum", testDataCheckSum),
+        ("testQueueMessage", testQueueMessage),
         ("testGetStatusMessages", testGetStatusMessages),
         ("testGetStatusMessagesResponse", testGetStatusMessagesResponse),
         ("testGetStatusIDDescription",testGetStatusIDDescription),
@@ -32,6 +33,34 @@ final class RDMMessageTest: XCTestCase {
         let data = Data([0xCC, 0x01, 0x19, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xCB, 0xA9, 0x87, 0x65, 0x43, 0x21, 0x00, 0x01, 0x00, 0x00, 0x00, 0x20, 0x00, 0x30, 0x01, 0x04, 0x06 , 0x6A])
         XCTAssertEqual(Checksum(data: data.prefix(data.count - 2)), 0x066A)
         XCTAssertEqual(Checksum(rawValue: 0x066A).description, "0x066A")
+    }
+    
+    func testQueueMessage() {
+        
+        let data = Data([0xCC, 0x01, 0x19, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xCB, 0xA9, 0x87, 0x65, 0x43, 0x21, 0x00, 0x01, 0x00, 0x00, 0x00, 0x20, 0x00, 0x20, 0x01, 0x04, 0x06 , 0x5A])
+        
+        let packet = RDM.Packet(
+            destination: DeviceUID(rawValue: "1234:56789ABC")!,
+            source: DeviceUID(rawValue: "CBA9:87654321")!,
+            transaction: 0,
+            typeField: 0x01,
+            messageCount: 0x00,
+            subDevice: .root,
+            messageData: .getQueueMessage(.init(status: .error))
+        )
+        
+        dump(packet)
+        
+        XCTAssertEqual(packet.data, data)
+        XCTAssert(packet.isChecksumValid)
+        
+        guard let decodedPacket = RDM.Packet(data: data)
+            else { XCTFail("Could not parse packet"); return }
+        XCTAssertEqual(packet, decodedPacket)
+        
+        guard let decodedFromPacketData = RDM.Packet(data: packet.data)
+            else { XCTFail("Could not parse packet"); return }
+        XCTAssertEqual(packet, decodedFromPacketData)
     }
     
     func testGetStatusMessages() {
