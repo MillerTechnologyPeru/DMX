13ba9b979a3d287c6db9612203d3c2640957cd72
Jurjdev <jurjdev@gmail.com>
2020-04-22 15:23:08 -0500

    Added `ResetDevice` Unit Tests

--- Tests/RDMTests/RDMMessageTest.swift
+++ Tests/RDMTests/RDMMessageTest.swift
@@ -132,6 +132,8 @@ final class RDMMessageTest: XCTestCase {
         ("testGetIdentifyDeviceResponse", testGetIdentifyDeviceResponse),
         ("testSetIdentifyDevice", testSetIdentifyDevice),
         ("testSetIdentifyDeviceResponse", testSetIdentifyDeviceResponse),
+        ("testResetDevice", testResetDevice),
+        ("testResetDeviceResponse", testResetDeviceResponse),
     ]
     
     func testDataCheckSum() {
@@ -4374,6 +4376,74 @@ final class RDMMessageTest: XCTestCase {
         XCTAssertEqual(packet.messageData.parameterID, .identifyDevice)
         XCTAssertEqual(packet.messageData.parameterDataLength, 0)
 
+        XCTAssertEqual(packet.data, data)
+        XCTAssert(packet.isChecksumValid)
+        
+        guard let decodedPacket = RDM.Packet(data: data)
+            else { XCTFail("Could not parse packet"); return }
+        XCTAssertEqual(packet, decodedPacket)
+        
+        guard let decodedFromPacketData = RDM.Packet(data: packet.data)
+            else { XCTFail("Could not parse packet"); return }
+        XCTAssertEqual(packet, decodedFromPacketData)
+    }
+    
+    func testResetDevice() {
+        
+        let data = Data([0xCC, 0x01, 0x19, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xCB, 0xA9, 0x87, 0x65, 0x43, 0x21, 0x00, 0x01, 0x00, 0x00, 0x00, 0x30, 0x10, 0x01, 0x01, 0xFF, 0x07, 0x56])
+        
+        let packet = RDM.Packet(
+            destination: DeviceUID(rawValue: "1234:56789ABC")!,
+            source: DeviceUID(rawValue: "CBA9:87654321")!,
+            transaction: 0,
+            portID: 1,
+            messageCount: 0,
+            subDevice: .root,
+            messageData: .resetDevice(.init(resetDevice: .coldReset))
+        )
+        
+        dump(packet)
+        
+        XCTAssertEqual(packet.messageData.commandClass, .set)
+        XCTAssertEqual(packet.messageData.parameterID, .resetDevice)
+        XCTAssertEqual(packet.messageData.parameterDataLength, 1)
+        
+        XCTAssertEqual(packet.data, data)
+        XCTAssert(packet.isChecksumValid)
+        
+        guard let messageData = MessageDataBlock(data: packet.messageData.data)
+            else { XCTFail("Could not parse Message Data Block"); return }
+        dump(messageData)
+        
+        guard let decodedPacket = RDM.Packet(data: data)
+            else { XCTFail("Could not parse packet"); return }
+        XCTAssertEqual(packet, decodedPacket)
+        
+        guard let decodedFromPacketData = RDM.Packet(data: packet.data)
+            else { XCTFail("Could not parse packet"); return }
+        XCTAssertEqual(packet, decodedFromPacketData)
+    }
+    
+    func testResetDeviceResponse() {
+        
+        let data = Data([0xCC, 0x01, 0x18, 0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xCB, 0xA9, 0x87, 0x65, 0x43, 0x21, 0x00, 0x00, 0x00, 0x00, 0x00, 0x31, 0x10, 0x01, 0x00, 0x06, 0x55])
+        
+        let packet = RDM.Packet(
+            destination: DeviceUID(rawValue: "1234:56789ABC")!,
+            source: DeviceUID(rawValue: "CBA9:87654321")!,
+            transaction: 0,
+            responseType: .acknowledgement,
+            messageCount: 0,
+            subDevice: .root,
+            messageData: .resetDeviceResponse
+        )
+        
+        dump(packet)
+        
+        XCTAssertEqual(packet.messageData.commandClass, .setResponse)
+        XCTAssertEqual(packet.messageData.parameterID, .resetDevice)
+        XCTAssertEqual(packet.messageData.parameterDataLength, 0)
+        
         XCTAssertEqual(packet.data, data)
         XCTAssert(packet.isChecksumValid)
         
